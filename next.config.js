356c22230a89c16987111c13bec16f77b4c6dff9
Nibsi3 <cameronfalck03@gmail.com>
2026-02-10 17:08:17 +0200

    fix: revert standalone output - breaks CSS serving on Hostinger

--- next.config.js
+++ next.config.js
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  output: 'standalone',
   headers: async () => [
     {
       source: '/_next/static/:path*',
